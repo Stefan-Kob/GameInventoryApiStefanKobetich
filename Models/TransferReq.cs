0f0ce8aabf3281177f00bdea3e15f0171e0ef548
Stefan <103537403+Stefan-Kob@users.noreply.github.com>
2026-02-25 21:15:37 -0500

    Removed file and added comment

--- Models/TransferReq.cs
+++ Models/TransferReq.cs
@@ -1,5 +1,6 @@
 namespace GameInventoryApiStefanKobetich.Models
 {
+    // Model representing a transfer request between two wallets
     public class TransferReq
     {
         public int FromId { get; set; }
